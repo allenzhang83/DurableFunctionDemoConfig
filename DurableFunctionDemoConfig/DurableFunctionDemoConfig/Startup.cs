af625cf7e9edbb2e27399b8486cef4cdbd53f0fb
Allen Zhang <allen.zhang1983713@gmail.com>
2020-05-31 21:20:42 +1200

    Initial commit

---
+++ DurableFunctionDemoConfig/DurableFunctionDemoConfig/Startup.cs
@@ -0,0 +1,23 @@
+﻿using DurableFunctionDemoConfig.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(DurableFunctionDemoConfig.Startup))]
+namespace DurableFunctionDemoConfig
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IGitHubApiService, GitHubApiService>();
+        }
+    }
+}
